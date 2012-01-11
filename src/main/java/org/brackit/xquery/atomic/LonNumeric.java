12dc24fdd2ce7a3b78f717063ec109f41516fcae
Sebastian Baechle <devnull@localhost>
2012-01-11 09:35:45 +0100

    update copyright notice

--- src/main/java/org/brackit/xquery/atomic/LonNumeric.java
+++ src/main/java/org/brackit/xquery/atomic/LonNumeric.java
@@ -1,6 +1,6 @@
 /*
  * [New BSD License]
- * Copyright (c) 2011, Brackit Project Team <info@brackit.org>  
+ * Copyright (c) 2011-2012, Brackit Project Team <info@brackit.org>  
  * All rights reserved.
  * 
  * Redistribution and use in source and binary forms, with or without
@@ -10,7 +10,7 @@
  *     * Redistributions in binary form must reproduce the above copyright
  *       notice, this list of conditions and the following disclaimer in the
  *       documentation and/or other materials provided with the distribution.
- *     * Neither the name of the <organization> nor the
+ *     * Neither the name of the Brackit Project Team nor the
  *       names of its contributors may be used to endorse or promote products
  *       derived from this software without specific prior written permission.
  * 
