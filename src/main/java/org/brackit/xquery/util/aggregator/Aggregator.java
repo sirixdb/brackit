19e671996bd1742987157666b8b85125f3932136
Sebastian Baechle <devnull@localhost>
2012-02-03 16:16:28 +0100

    generalized aggregate functions like sum, avg, min, and max for more flexible use

--- src/main/java/org/brackit/xquery/util/aggregator/Aggregator.java
+++ src/main/java/org/brackit/xquery/util/aggregator/Aggregator.java
@@ -17,54 +17,26 @@
  * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
  * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
- * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR
- * ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ * DISCLAIMED. IN NO EVENT SHALL <COPYRIGHT HOLDER> BE LIABLE FOR ANY
+ * DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
  * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
  * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
  * ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
  * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
  * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
-package org.brackit.xquery.function.fn;
+package org.brackit.xquery.util.aggregator;
 
-import org.brackit.xquery.QueryContext;
 import org.brackit.xquery.QueryException;
-import org.brackit.xquery.atomic.Int32;
-import org.brackit.xquery.atomic.QNm;
-import org.brackit.xquery.function.AbstractFunction;
-import org.brackit.xquery.module.StaticContext;
-import org.brackit.xquery.util.aggregator.SumAvgAggregator;
 import org.brackit.xquery.xdm.Sequence;
-import org.brackit.xquery.xdm.Signature;
 
 /**
- * 
  * @author Sebastian Baechle
  * 
  */
-public class SumAvg extends AbstractFunction {
-	private final boolean avg;
+public interface Aggregator {
+	public Sequence getAggregate() throws QueryException;
 
-	public SumAvg(QNm name, Signature signature, boolean avg) {
-		super(name, signature, true);
-		this.avg = avg;
-	}
+	public void add(Sequence seq) throws QueryException;
 
-	@Override
-	public Sequence execute(StaticContext sctx, QueryContext ctx,
-			Sequence[] args) throws QueryException {
-		Sequence seq = args[0];
-
-		Sequence defaultValue = (args.length == 2) ? args[1] : Int32.ZERO;
-		if (seq == null) {
-			if (avg) {
-				return null;
-			}
-			return defaultValue;
-		}
-
-		SumAvgAggregator agg = new SumAvgAggregator(avg, defaultValue);
-		agg.add(seq);
-		return agg.getAggregate();
-	}
 }
