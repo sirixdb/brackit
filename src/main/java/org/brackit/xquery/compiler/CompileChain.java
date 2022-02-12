c2f819034f046a857137b1da4479735139537115
Johannes Lichtenberger <johannes.lichtenberger@uni-konstanz.de>
2022-02-12 01:37:51 +0100

    Update CompileChain.java

--- src/main/java/org/brackit/xquery/compiler/CompileChain.java
+++ src/main/java/org/brackit/xquery/compiler/CompileChain.java
@@ -1,14 +1,8 @@
 /*
  * [New BSD License]
-<<<<<<< HEAD
  * Copyright (c) 2011-2012, Brackit Project Team <info@brackit.org>
  * All rights reserved.
  *
-=======
- * Copyright (c) 2011-2012, Brackit Project Team <info@brackit.org>
- * All rights reserved.
- *
->>>>>>> upstream/master
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions are met:
  *     * Redistributions of source code must retain the above copyright
@@ -19,11 +13,8 @@
  *     * Neither the name of the Brackit Project Team nor the
  *       names of its contributors may be used to endorse or promote products
  *       derived from this software without specific prior written permission.
-<<<<<<< HEAD
  *
-=======
  *
->>>>>>> upstream/master
  * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
  * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
