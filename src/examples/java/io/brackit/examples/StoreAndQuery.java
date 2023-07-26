7a8984819e8e14280e81916d046acc47a1a89741
johannesl <lichtenberger.johannes@gmail.com>
2023-07-26 22:12:23 +0200

    Rename example package, too...

--- src/examples/java/io/brackit/examples/StoreAndQuery.java
+++ src/examples/java/io/brackit/examples/StoreAndQuery.java
@@ -25,7 +25,7 @@
  * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
  * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
-package org.brackit.examples;
+package io.brackit.examples;
 
 import io.brackit.query.BrackitQueryContext;
 import io.brackit.query.QueryContext;
